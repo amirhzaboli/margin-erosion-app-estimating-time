9b6a4e95a322b042d71341d353d9def6b84df917
amirhzaboli <amir.zaboli@thynkgroup.com.au>
2025-07-23 10:22:30 +1000

    Update margin_erosion_app.py

--- margin_erosion_app.py
+++ margin_erosion_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import os
 
-# Load the trained model
-with open("rf_model.pkl", "rb") as f:
+# Load the trained model using absolute path
+model_path = os.path.join(os.path.dirname(__file__), "rf_model.pkl")
+with open(model_path, "rb") as f:
     model = pickle.load(f)
 
 st.title("🧱 Margin Erosion Predictor")
